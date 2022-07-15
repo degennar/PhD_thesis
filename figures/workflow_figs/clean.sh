8a4e29e6660db2e7ce7f47df734c8523b3539917
degennar <riccardo.degennar@gmail.com>
2022-07-15 11:43:28 +0200

    updating workflow figures

--- figures/workflow_figs/clean.sh
+++ figures/workflow_figs/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f --verbose workflow.aux* workflow.fdb_latexmk workflow.fls workflow.log workflow.nav workflow.out workflow.snm workflow.toc workflow.synctex.gz workflow.pdf
-rm -f --verbose workflow-figure*.md5 workflow-figure*.log workflow-figure*.dpth workflow-figure*.dpth workflow-figure*.pdf
+rm -f --verbose workflow-figure*.md5 workflow-figure*.log workflow-figure*.dpth workflow-figure*.dpth
