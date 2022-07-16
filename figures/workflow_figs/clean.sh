0f733b080a697a19ef2a5bcea6f49d73b60e70c7
Riccardo De Gennaro <riccardo.degennar@gmail.com>
2022-07-16 20:41:13 +0200

    converting links to actual files for compliance with Overleaf

--- figures/workflow_figs/clean.sh
+++ figures/workflow_figs/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f --verbose workflow.aux* workflow.fdb_latexmk workflow.fls workflow.log workflow.nav workflow.out workflow.snm workflow.toc workflow.synctex.gz workflow.pdf
-rm -f --verbose workflow-figure*.md5 workflow-figure*.log workflow-figure*.dpth workflow-figure*.dpth
+rm -f -v workflow.aux* workflow.fdb_latexmk workflow.fls workflow.log workflow.nav workflow.out workflow.snm workflow.toc workflow.synctex.gz workflow.pdf
+rm -f -v workflow-figure*.md5 workflow-figure*.log workflow-figure*.dpth workflow-figure*.dpth
--- figures/workflow_figs/compile.sh
+++ figures/workflow_figs/compile.sh
@@ -2,3 +2,6 @@
 
 sh clean.sh
 pdflatex --shell-escape workflow.tex
+
+cp workflow-figure0.pdf ../dscf_workflow.pdf
+cp workflow-figure1.pdf ../dfpt_workflow.pdf
